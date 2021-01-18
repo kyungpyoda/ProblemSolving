ac49dd9a6d6fd5d9543dccc88cf1c9a3d32ee777
kyungpyoda <qorghdl@naver.com>
2021-01-19 00:31:19 +0900

    Implement 프로그래머스 문자열을 정수로 바꾸기

---
+++ ProblemSolving/ProblemSolving/Programmers_문자열을정수로바꾸기.swift
@@ -0,0 +1,14 @@
+//
+//  Programmers_문자열을정수로바꾸기.swift
+//  ProblemSolving
+//
+//  Created by 홍경표 on 2021/01/19.
+//
+
+import Foundation
+
+class Programmers_문자열을정수로바꾸기 {
+    func solution(_ s:String) -> Int {
+        return Int(s)!
+    }
+}

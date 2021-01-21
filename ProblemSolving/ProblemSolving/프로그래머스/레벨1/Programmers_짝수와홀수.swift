77c6672364d26d82234d99b068f6aee631067520
kyungpyoda <qorghdl@naver.com>
2021-01-21 21:38:32 +0900

    Implement 프로그래머스 짝수와 홀수

---
+++ ProblemSolving/ProblemSolving/프로그래머스/레벨1/Programmers_짝수와홀수.swift
@@ -0,0 +1,14 @@
+//
+//  Programmers_짝수와홀수.swift
+//  ProblemSolving
+//
+//  Created by 홍경표 on 2021/01/21.
+//
+
+import Foundation
+
+class Programmers_짝수와홀수 {
+    func solution(_ num:Int) -> String {
+        return num % 2 == 0 ? "Even" : "Odd"
+    }
+}

7b5244a4e0568c8ffe923e309f7eae36b1912d3f
kyungpyoda <qorghdl@naver.com>
2021-02-06 09:32:10 +0900

    Update Programmers_문자열내p와y의개수.swift

--- ProblemSolving/ProblemSolving/프로그래머스/레벨1/Programmers_문자열내p와y의개수.swift
+++ ProblemSolving/ProblemSolving/프로그래머스/레벨1/Programmers_문자열내p와y의개수.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 
-func solution(_ s:String) -> Bool {
-    var count = 0
-    s.forEach {
-        if $0 == "p" || $0 == "P" {
-            count += 1
-        } else if $0 == "y" || $0 == "Y" {
-            count -= 1
+class Programmers_문자열내p와y의개수 {
+    func solution(_ s:String) -> Bool {
+        var count = 0
+        s.forEach {
+            if $0 == "p" || $0 == "P" {
+                count += 1
+            } else if $0 == "y" || $0 == "Y" {
+                count -= 1
+            }
         }
+        return count == 0
     }
-    return count == 0
+
 }

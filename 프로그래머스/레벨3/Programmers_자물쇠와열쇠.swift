9739977f0aaa9c21aea9790041e99f85d8e94451
kyungpyoda <qorghdl@naver.com>
2021-02-26 20:07:42 +0900

    Implement 프로그래머스 자물쇠와 열쇠

--- 프로그래머스/레벨3/Programmers_자물쇠와열쇠.swift
+++ 프로그래머스/레벨3/Programmers_자물쇠와열쇠.swift
@@ -8,7 +8,49 @@
 import Foundation
 
 class Programmers_자물쇠와열쇠 {
+    var M = 0
+    var N = 0
     func solution(_ key:[[Int]], _ lock:[[Int]]) -> Bool {
+        M = key.count
+        N = lock.count
+        for rMove in 1-M...N-1 {
+            for cMove in 1-M...N-1 {
+                var key = key
+                if unlock(key: key, lock: lock, rMove: rMove, cMove: cMove) {
+                    return true
+                }
+                key = rotate(arr: key)
+                if unlock(key: key, lock: lock, rMove: rMove, cMove: cMove) {
+                    return true
+                }
+                key = rotate(arr: key)
+                if unlock(key: key, lock: lock, rMove: rMove, cMove: cMove) {
+                    return true
+                }
+                key = rotate(arr: key)
+                if unlock(key: key, lock: lock, rMove: rMove, cMove: cMove) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    func unlock(key: [[Int]], lock: [[Int]], rMove: Int, cMove: Int) -> Bool {
+        for r in 0..<lock.count {
+            for c in 0..<lock.count {
+                let k = (0..<M ~= r-rMove && 0..<M ~= c-cMove) ? key[r-rMove][c-cMove] : 0
+                guard lock[r][c] ^ k == 1 else { return false }
+            }
+        }
         return true
+    }
+    func rotate(arr: [[Int]]) -> [[Int]] {
+        let rCount = arr.count
+        let cCount = arr[0].count
+        return (0..<cCount).map { c in
+            (0..<rCount).map { r in
+                arr[r][c]
+            }.reversed()
+        }
     }
 }
